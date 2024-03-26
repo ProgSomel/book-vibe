643a57966b85e6d3e83d125c4a9c85ab8d94ef1c
ProgSomel <somelahmed88@gmail.com>
2024-03-26 21:11:56 +0600

    error page added

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -7,11 +7,13 @@ import Home from "../Pages/Home/Home/Home";
 import BookDetails from "../Pages/BookDetails/BookDetails";
 import ListedBooks from "../Pages/ListedBooks/ListedBooks";
 import PagesToRead from "../Pages/PagesToRead/PagesToRead";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Root></Root>,
+      errorElement: <ErrorPage></ErrorPage>,
       children: [
         {
             path: "/",
